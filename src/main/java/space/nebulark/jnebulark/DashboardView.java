b2f22b10f0792c8d35faeb124f619625522a613d
Jakob Waibel <jakob.waibel@web.de>
2020-12-11 20:07:04 +0000

    feat: Add accordion

--- src/main/java/space/nebulark/jnebulark/DashboardView.java
+++ src/main/java/space/nebulark/jnebulark/DashboardView.java
@@ -4,5 +4,19 @@ import org.teavm.flavour.templates.BindTemplate;
 
 @BindTemplate("templates/dashboard.html")
 public class DashboardView {
-    
+   public boolean visible = true;
+
+   public void visible() {
+       visible = !visible;
+       System.out.println("Hallo");
+   }
+
+   public boolean getVisible() {
+        System.out.println(visible);
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
 }
