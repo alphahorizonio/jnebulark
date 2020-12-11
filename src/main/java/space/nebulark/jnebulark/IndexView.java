0837f0d1096ec2f9bedfeb7e5647c9e8630b24bd
Jakob Waibel <jakob.waibel@web.de>
2020-12-11 15:45:42 +0000

    feat: Add link to "New Nebula" button

--- src/main/java/space/nebulark/jnebulark/IndexView.java
+++ src/main/java/space/nebulark/jnebulark/IndexView.java
@@ -1,5 +1,8 @@
 package space.nebulark.jnebulark;
 
+import java.util.function.Consumer;
+
+import org.teavm.flavour.routing.Routing;
 import org.teavm.flavour.templates.BindTemplate;
 
 @BindTemplate("templates/index.html")
@@ -9,6 +12,7 @@ public class IndexView {
 
     public void next() {
         System.out.println("New Nebula");
+        Routing.open(IRoute.class).missionControl();
     }
 
     public String getNebulaID() {
@@ -18,5 +22,9 @@ public class IndexView {
 
     public void setNebulaID(String nebulaID) {
         this.nebulaID = nebulaID;
+    }
+
+    public IRoute route(Consumer<String> consumer) {
+        return Routing.build(IRoute.class, consumer);
     }
 }
