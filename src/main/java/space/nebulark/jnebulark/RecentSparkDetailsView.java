4302e727c20611b56be400d281a3b0e29991efe4
Jakob Waibel <jakob.waibel@web.de>
2020-12-13 22:06:55 +0000

    feats: Add links to sparkDetails and recentSparkDetails

--- src/main/java/space/nebulark/jnebulark/RecentSparkDetailsView.java
+++ src/main/java/space/nebulark/jnebulark/RecentSparkDetailsView.java
@@ -1,7 +1,11 @@
 package space.nebulark.jnebulark;
 
+import org.teavm.flavour.routing.Routing;
 import org.teavm.flavour.templates.BindTemplate;
 @BindTemplate("templates/recentSparkDetails.html")
 public class RecentSparkDetailsView {
 
+    public void toDashboard() {
+        Routing.open(IRoute.class).dashboard();
+    }
 }
