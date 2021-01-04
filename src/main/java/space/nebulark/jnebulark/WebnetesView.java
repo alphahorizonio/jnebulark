11d455872f8f056fd107206cf04b10a1d03951b1
lucas-goldner <lucas.goldner@googlemail.com>
2021-01-04 15:29:40 +0100

    feat: Placeholder functions

--- src/main/java/space/nebulark/jnebulark/WebnetesView.java
+++ src/main/java/space/nebulark/jnebulark/WebnetesView.java
@@ -2,8 +2,28 @@ package space.nebulark.jnebulark;
 
 import org.teavm.flavour.routing.Routing;
 import org.teavm.flavour.templates.BindTemplate;
+import org.teavm.jso.typedarrays.Uint8Array;
 
 @BindTemplate("templates/webnetes.html")
 public class WebnetesView {
 
+    public void close() {
+        return;
+    }
+
+    public String createRessources(String ressources) {
+        return ressources;
+    }
+
+    public void deleteRessources(String ressources) {
+        return;
+    }
+
+    public String open(String ressources) {
+        return ressources;
+    }
+
+    public String seedFile(String label, String name, String repository, Uint8Array fileInstance) {
+        return "seededFile";
+    }
 }
