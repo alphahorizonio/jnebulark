7adcdba86dc0943d6c0edd889a1f20b4cc08eb89
lucas-goldner <lucas.goldner@googlemail.com>
2021-01-11 22:55:28 +0100

    feat: Pure java modal

--- src/main/java/space/nebulark/jnebulark/DashboardView.java
+++ src/main/java/space/nebulark/jnebulark/DashboardView.java
@@ -5,19 +5,19 @@ import org.teavm.flavour.templates.BindTemplate;
 
 @BindTemplate("templates/dashboard.html")
 public class DashboardView {
-   public boolean visible = true;
+    public boolean visible = true;
 
-   public void visible() {
-       visible = false;
-       System.out.println("Hallo");
-   }
+    public void visible() {
+        visible = false;
+        System.out.println("Hallo");
+    }
 
-   public void invisible() {
-       visible = true;
-       System.out.println("invis");
-   }
+    public void invisible() {
+        visible = true;
+        System.out.println("invis");
+    }
 
-   public boolean getVisible() {
+    public boolean getVisible() {
         System.out.println(visible);
         return visible;
     }
--- src/main/java/space/nebulark/jnebulark/WebnetesOverviewView.java
+++ src/main/java/space/nebulark/jnebulark/WebnetesOverviewView.java
@@ -1,13 +1,19 @@
 package space.nebulark.jnebulark;
 
+import javax.swing.text.Document;
+
+import com.google.inject.spi.Element;
+
 import org.teavm.flavour.routing.Routing;
 import org.teavm.flavour.templates.BindTemplate;
-import org.teavm.jso.JSBody;
 import org.teavm.jso.browser.Window;
+import org.teavm.jso.dom.html.HTMLElement;
 import org.teavm.jso.typedarrays.Uint8Array;
 
 @BindTemplate("templates/webnetesOverview.html")
 public class WebnetesOverviewView {
+    public boolean visible = true;
+    public boolean createVisible = true;
 
     public void toWebnetesOverview() {
         Routing.open(IRoute.class).webnetesOverview();
@@ -24,6 +30,16 @@ public class WebnetesOverviewView {
     public void toCluster() {
         Window window = Window.current();
         window.getLocation().replace("https://webnetesctl.vercel.app/");
+    }
+
+    public void setHamburgerVisible() {
+        boolean otherVisible = !visible;
+        this.visible = otherVisible;
+    }
+
+    public void setCreateModalVisible() {
+        boolean otherVisible = !createVisible;
+        this.createVisible = otherVisible;
     }
 
     public void printMe(String message) {
